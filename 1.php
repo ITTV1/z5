245537bdf72557469ba7f1539a0fa0562ed0bba2
ITTV1 <62187261+ITTV1@users.noreply.github.com>
2020-04-05 18:01:39 +0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,54 @@
+<?php
+
+//Математические функции
+
+
+
+//mixed abs(mixed $num)
+
+//
+//double round(double $value, [, int $pre = 0, int mode = PHP_ROUND_HALF_UP])
+//
+//
+//0.5
+//$f = round(4.4);
+//$f = round(5.5);
+//$f = round(5.6);
+//
+//
+//$f = round(125.156, 2);
+//
+//$f = round(132.256, 2);
+//echo $f;
+//
+//
+//PHP_ROUND_HALF_ODD
+//PHP_ROUND_HALF_EVEN
+//PHP_ROUND_HALF_DOWN
+//PHP_ROUND_HALF_UP
+
+
+
+
+
+//int ceil(float $num)
+//$f = ceil(4.1);
+//
+//$f = ceil(5);
+//
+//$f = ceil(5.6);
+//echo $f;
+
+
+// int  floor(float $num)
+
+$f = floor(4.9);
+echo $f;
+
+$f = floor(5);
+
+$f = floor(5.6);
+
+
+
+?>
